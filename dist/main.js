884eef6a24907a62b3cd16c2ef69961d02692e04
Jovohir <java6923@mail.ru>
2025-09-05 00:08:57 +0500

    swagger

--- dist/main.js
+++ dist/main.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
+const swagger_1 = require("@nestjs/swagger");
 async function bootstrap() {
     const PORT = Number(process.env.PORT) || 3000;
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
@@ -13,8 +14,15 @@ async function bootstrap() {
         forbidNonWhitelisted: true,
         errorHttpStatusCode: common_1.HttpStatus.UNPROCESSABLE_ENTITY,
     }));
+    const config = new swagger_1.DocumentBuilder()
+        .setTitle('University API')
+        .setDescription('University Management System')
+        .setVersion('1.0')
+        .build();
+    const document = swagger_1.SwaggerModule.createDocument(app, config);
+    swagger_1.SwaggerModule.setup('api/docs', app, document);
     await app.listen(PORT);
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server: http://localhost:${PORT}`);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpStatus, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = Number(process.env.PORT) || 3000;
   const app = await NestFactory.create(AppModule);
-  
+
   app.setGlobalPrefix('/api');
   app.useGlobalPipes(
     new ValidationPipe({
@@ -15,8 +16,17 @@ async function bootstrap() {
       errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
     }),
   );
-  
+
+  const config = new DocumentBuilder()
+    .setTitle('University API')
+    .setDescription('University Management System')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(PORT);
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server: http://localhost:${PORT}`);
 }
 bootstrap();
